33ce7446d2d0ec85bc12f31db3634bcef8ee7c6f
BabaSwaroopa07 <32baba2000@gmail.com>
2022-02-01 18:33:51 +0530

    Final Case Study Submission

---
+++ Final Check/Final Check/MyFoodSupply/FoodDetailTesting/FoodItemTests.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+using System.IO;
+using System;
+using MyFoodSupply;
+
+namespace FoodDetailTesting
+{
+    [TestFixture]
+    public class FoodItemTests
+    {
+        Program obj = new Program();
+
+        
+        [TestCase("Chocolates",2,"12/12/2022", 23.23)]
+        [TestCase("Blueberry", 2, "12/10/2022", 21000.00)]
+        public void FoodDetailCreate(string name, int dishType, DateTime expiryDate, double price)
+        {
+            Assert.DoesNotThrow(() => obj.CreateFoodDetail(name,dishType,expiryDate,price));
+        }
+
+        
+        [TestCase("", 2, "12/12/2021", 23.23)]
+        [TestCase("", 2, "12/10/2021", 21000.00)]
+        public void ExceptionEmptyName(string name, int dishType, DateTime expiryDate, double price)
+        {
+
+            var ex = Assert.Throws<Exception>(() => obj.CreateFoodDetail(name, dishType, expiryDate, price));
+            Assert.That(ex.Message, Is.EqualTo("Please provide valid foodItem"));
+        }
+
+        [TestCase("chocolates", 2, "12/12/2021", -0.00001)]
+        [TestCase("Blueberry", 2, "12/10/2021", -10000)]
+        public void NegativePriceException(string name, int dishType, DateTime expiryDate, double price)
+        {
+
+            var ex = Assert.Throws<Exception>(() => obj.CreateFoodDetail(name, dishType, expiryDate, price));
+            Assert.That(ex.Message, Is.EqualTo("Please check the valid value"));
+        }
+
+        [TestCase("chocolates", 2, "12/12/2020", 200)]
+        [TestCase("Blueberry", 2, "12/10/2000",22.22)]
+        public void ExpiryDate(string name, int dishType, DateTime expiryDate, double price)
+        {
+
+            var ex = Assert.Throws<Exception>(() => obj.CreateFoodDetail(name, dishType, expiryDate, price));
+            Assert.That(ex.Message, Is.EqualTo("Please provide valid date"));
+        }
+
+    }
+}
---
+++ Final Check/Final Check/MyFoodSupply/SupplyDetailsTesting/SupplyDetailsTests.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using System.IO;
+using System;
+using MyFoodSupply;
+
+namespace SupplyDetailsTesting
+{
+    public class SupplyDetailsTests
+    {
+        Program obj = new Program();
+
+
+        [TestCase(2, "12/10/2022", "Hydi",23.22)]
+        [TestCase(10, "12/11/2022", "Klara",45)]
+        public void CartonObjectCreation(int foodItemCount, DateTime requestDate, string sellerName, double packingCharge)
+        {
+            FoodDetail food = obj.CreateFoodDetail("Blueberry", 2, DateTime.Parse("12/12/2022"), 21000.00);
+            Assert.DoesNotThrow(() => obj.CreateSupplyDetail(foodItemCount,requestDate,sellerName,packingCharge,food));
+            
+        }
+
+        [TestCase(-2, "12/10/2021", "Hydi", 23.22)]
+        [TestCase(-10, "12/11/2021", "Klara", 45)]
+        public void LessThanZeroFoodItemCount(int foodItemCount, DateTime requestDate, string sellerName, double packingCharge)
+        {
+            FoodDetail food = obj.CreateFoodDetail("Blueberry", 2, DateTime.Parse("12/12/2022"), 21000.00);
+
+            var ex = Assert.Throws<Exception>(() => obj.CreateSupplyDetail(foodItemCount, requestDate, sellerName, packingCharge, food));
+            Assert.That(ex.Message, Is.EqualTo("Please check the Food item which is selected"));
+        }
+
+        [TestCase(2, "12/10/2023", "Hydi", 23.22)]
+        [TestCase(10, "12/11/2023", "Klara", 45)]
+        public void RequestDateValidity(int foodItemCount, DateTime requestDate, string sellerName, double packingCharge)
+        {
+            FoodDetail food = obj.CreateFoodDetail("Blueberry", 2, DateTime.Parse("12/12/2022"), 21000.00);
+
+            var ex = Assert.Throws<Exception>(() => obj.CreateSupplyDetail(foodItemCount, requestDate, sellerName, packingCharge, food));
+            Assert.That(ex.Message, Is.EqualTo("Please check the request Date"));
+        }
+
+        [TestCase(2, "12/10/2021", "Hydi", 23.22)]
+        [TestCase(10, "12/11/2021", "Klara", 45)]
+        public void NullSupplyObject(int foodItemCount, DateTime requestDate, string sellerName, double packingCharge)
+        {
+            Assert.AreEqual(obj.CreateSupplyDetail(foodItemCount,requestDate,sellerName,packingCharge,null), null);
+        }
+
+
+    }
+}
